be12bf94f9289d4a41ceb81c47f27ac47a4b6a11
Shashank Gupta <shashankgupta9248@gmail.com>
2023-04-23 13:40:54 +0530

    updated

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,14 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
     try {
-        res.status(200).json({msg: "Products testing route"});
+        const products = await Product.find(req.query)
+        console.log(req.query)
+        if(products.length === 0){
+          res.send("No products in the database")
+        }
+        else{
+          res.status(200).json({ products, nbHits: products.length });
+        }
     } catch (error) {
         console.log(error)
     }
--- models/product.js
+++ models/product.js
@@ -1,11 +1,39 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const productSchema = mongoose.Schema({
-    
-//         name: String,
-//         required: true
-    
-// })
+const productSchema = new mongoose.Schema({
+  name: {
+    type: [String, "Please enter a valid name"],
+    required: [true, "Please enter your name"],
+  },
 
-// const Product = new mongoose.model('Products', productSchema)
-// module.exports = Product
+  price: {
+    type: Number,
+    required: [true, "Please enter the price"],
+  },
+
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: '{VALUE} is not supported'
+    },
+  },
+});
+
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product
